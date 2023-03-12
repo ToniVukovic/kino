16d101093b6b7292e8853a71bea48d91caace4bf
Toni Vukovic <tonivukovic1204@gmail.com>
2023-03-12 03:13:05 +0100

    add and style button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ function App() {
   return (
     <div>
       <div className="card-container">
+        <Card pretpremijera />
         <Card pretpremijera sink />
         <Card titl revija />
+        <Card sink />
+      </div>
+      <div className="ticket-flex">
+        <button className="ticket-btn sheen">
+          <div className="ticket-box-2"></div>
+          <div className="ticket-box-1"></div>
+          ULAZNICE
+        </button>
       </div>
     </div>
   );
